05daae944cae1386251e39125bc9f93fa8189764
remlostime <remlostime@gmail.com>
2017-02-16 09:15:16 -0800

    Add New Pods
    * ActiveLabel
    * SCLAlertView
    * Remove ‘KILabel’

--- one/Pods/Target Support Files/KILabel/KILabel-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "KILabel.h"
-
-FOUNDATION_EXPORT double KILabelVersionNumber;
-FOUNDATION_EXPORT const unsigned char KILabelVersionString[];
-
